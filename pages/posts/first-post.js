a180b74db4350b0cd9080f9745dae180206f05ec
fluffyfrog02 <sungbean.paik@gmail.com>
2024-04-07 18:06:32 +0900

    Initial commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>hello i'm 정보시스템학과 백성빈</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
+}
